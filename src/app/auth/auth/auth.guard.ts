d023bcf91c5d7af7f3d76e9a506b05351c697b95
rakitenkogermans <rakitenko.germans@gmail.com>
2021-09-18 22:15:40 +0300

    Auto login & auto logout. Protect recipes with canActivate.

---
+++ src/app/auth/auth/auth.guard.ts
@@ -0,0 +1,21 @@
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {AuthService} from './auth.service';
+import {map, take, tap} from 'rxjs/operators';
+
+@Injectable({providedIn: 'root'})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {
+  }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+    return this.authService.user.pipe(take(1), map(user => {
+      return !!user;
+    }),
+      tap(isAuth => {
+        if (!isAuth) {
+          this.router.navigate(['/auth']);
+        }
+    }));
+  }
+}
