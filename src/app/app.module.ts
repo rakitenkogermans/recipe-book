586bcfb5883c35a0b3a4e26c00838ff04526b1bb
rakitenkogermans <rakitenko.germans@gmail.com>
2021-09-20 18:04:08 +0300

    Alert modal component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { AuthComponent } from './auth/auth/auth.component';
 import {AuthService} from './auth/auth/auth.service';
 import {LoadingSpinnerComponent} from './shared/loading-spinner/loading-spinner.component';
 import {AuthInterceptorService} from './auth/auth/auth-interceptor.service';
+import { AlertComponent } from './shared/alert/alert/alert.component';
 
 @NgModule({
   declarations: [
@@ -38,7 +39,8 @@ import {AuthInterceptorService} from './auth/auth/auth-interceptor.service';
     RecipeStartComponent,
     RecipeEditComponent,
     AuthComponent,
-    LoadingSpinnerComponent
+    LoadingSpinnerComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
